1066ca6924a33d16dbb081a61c04c848b317e63b
mapledrive <sait.sharifullin@gmail.com>
2024-03-01 02:20:22 +0500

    comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import getAuthorsRequest from "./api/authors/getAuthorsRequest";
+import getCommentsRequest from "./api/comments/getCommentsRequest";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+interface IAuthor {
+    id: number;
+    name: string;
+    avatar: string;
 }
 
+interface IComments {
+    id: number;
+    created: string;
+    text: string;
+    author: number;
+    parent: number;
+    likes: number;
+}
+
+const App: React.FC = () => {
+    const [authors, setAuthors] = useState<IAuthor[]>([]);
+    const [comments, setComments] = useState<IComments[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [commentError, setCommentError] = useState(null);
+
+    useEffect(() => {
+        async function fetchCommentData() {
+            try {
+                const data = await getCommentsRequest(1);
+                console.log(data, "comments");
+                setComments(data.data);
+                setLoading(false);
+            } catch (error) {
+                //setCommentError(error);
+                setLoading(false);
+            }
+        }
+
+        fetchCommentData();
+    }, []);
+
+    useEffect(() => {
+        async function fetchAuthorsData() {
+            try {
+                const data = await getAuthorsRequest();
+                console.log(data, "zalupa");
+                setAuthors(data);
+                setLoading(false);
+            } catch (error) {
+                //setError(error);
+                setLoading(false);
+            }
+        }
+
+        fetchAuthorsData();
+    }, []);
+
+    const results = comments.filter((obj) => {
+        return obj.parent === null;
+    });
+
+    return (
+        <div className="App-header">
+            <div className="wrapper">
+                {`${comments.length} comments`}
+                <hr />
+                {results.map((post) => (
+                    <Comment
+                        key={post.id}
+                        {...post}
+                        {...authors[post.author]}
+                    />
+                ))}
+            </div>
+            <button className="load">загрузить еще</button>
+        </div>
+    );
+};
+
 export default App;
+
+const Comment: React.FC = (props: any) => {
+    let commentDate = new Date(props.created);
+    return (
+        <div className="comment-wrap">
+            <div className="holder">
+                <div className="col1">
+                    <div className="loaded">
+                        <img className="pic" src={`${props.avatar}`} />
+                    </div>
+                </div>
+                <div className="col2">
+                    {props.name}
+                    <br />
+                    <p className="darken">{commentDate.toLocaleString()}</p>
+                </div>
+            </div>
+            <div className="holder">
+                <div className="col1">
+                    <div className="loaded"></div>
+                </div>
+                <div className="col2">{props.text}</div>
+            </div>
+        </div>
+    );
+};
